1db8df5b4d2312927b3dbe7471d557c4ead06838
Ilya <ilyaefish@gmail.com>
2018-06-02 12:27:32 +0300

    set port as integer

--- repo/ServerUdp.py
+++ repo/ServerUdp.py
@@ -9,7 +9,7 @@ args = vars(parser.parse_args())
 # Here we define the UDP IP address as well as the port number that we have
 # already defined in the client python script.
 UDP_IP_ADDRESS = args['ip']
-UDP_PORT_NO = args['port']
+UDP_PORT_NO = int(args['port'])
 # declare our serverSocket upon which
 # we will be listening for UDP messages
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,7 +20,7 @@ count = 0
 DATA = []
 while True:
     data, addr = serverSock.recvfrom(1024)
-    print "Message: "+str(count), data
-    count = count+1
-    if count %3 ==0:
+    print "Message: " + str(count), data
+    count = count + 1
+    if count % 3 == 0:
         print "Now I want to Send it"
