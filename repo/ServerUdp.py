4096b9431f830e467a69559ceadfe16a58ffa68d
Ilya <ilyaefish@gmail.com>
2018-06-02 19:54:31 +0300

    commit

--- repo/ServerUdp.py
+++ repo/ServerUdp.py
@@ -1,26 +1,48 @@
 import argparse
 import socket
 
-parser = argparse.ArgumentParser(description='Ip and port')
-parser.add_argument('-i', '--ip', help='The ip of the Server', required=False, default="0.0.0.0")
-parser.add_argument('-p', '--port', help='The port of the server', required=False, default=5000)
-args = vars(parser.parse_args())
 
-# Here we define the UDP IP address as well as the port number that we have
-# already defined in the client python script.
-UDP_IP_ADDRESS = args['ip']
-UDP_PORT_NO = int(args['port'])
-# declare our serverSocket upon which
-# we will be listening for UDP messages
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# One difference is that we will have to bind our declared IP address
-# and port number to our newly declared serverSock
-serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-count = 0
-DATA = []
-while True:
-    data, addr = serverSock.recvfrom(1024)
-    print "Message: " + str(count), data
-    count = count + 1
-    if count % 3 == 0:
-        print "Now I want to Send it"
+def send_udp(ip, port, message):
+    udp_ip_address = ip
+    udp_port_no = port
+    Message = message
+    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSock.sendto(Message, (udp_ip_address, udp_port_no))
+
+
+def buffer_manipulation():
+    print "manipulate"
+    # TODO: add you manipulation logic
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Ip and port')
+    parser.add_argument('-i', '--ip', help='The ip of the Server', required=False, default="0.0.0.0")
+    parser.add_argument('-p', '--port', help='The port of the server', required=False, default=5001)
+    parser.add_argument('--phone_ip', help='The ip of the Server', required=False, default="0.0.0.0")
+    parser.add_argument('--phone_port', help='The port of the server', required=False, default=5002)
+    parser.add_argument('--debug', action='store_true', help='for Data and count play')
+    args = vars(parser.parse_args())
+    udp_ip_address = args['ip']
+    udp_port_no = int(args['port'])
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((udp_ip_address, udp_port_no))
+    count = 0
+    dlist = []
+
+    while True:
+
+        data, addr = server_socket.recvfrom(1024)
+        print "Message: " + str(count), data
+        count = count + 1
+        dlist.append(data + ": " + str(count))
+        if count % 3 == 0:
+            print "Now I want to Send it"
+            udpBuffer = dlist[:-4:-1]
+            for i in udpBuffer:
+                send_udp(ip=args['phone_ip'], port=int(args['phone_port']), message=i)
+            udpBuffer[:] = []
+
+
+if __name__ == '__main__':
+    main()
