1db8f5d3f30ef11b69d83c1c4b305f1fca4084dd
Ilya <ilyaefish@gmail.com>
2018-06-02 12:45:37 +0300

    local host

--- repo/ServerUdp.py
+++ repo/ServerUdp.py
@@ -2,7 +2,7 @@ import argparse
 import socket
 
 parser = argparse.ArgumentParser(description='Ip and port')
-parser.add_argument('-i', '--ip', help='The ip of the Server', required=False, default="127.0.0.1")
+parser.add_argument('-i', '--ip', help='The ip of the Server', required=False, default="0.0.0.0")
 parser.add_argument('-p', '--port', help='The port of the server', required=False, default=5000)
 args = vars(parser.parse_args())
 
